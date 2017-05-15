7632ef9ee2a125d2eb0344a857f92e89e9587f5e
Victor <1341634255@qq.com>
2017-05-15 19:26:33 +0800

    Draft Editor init

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import MyEditor from './MyEditor';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to Draft Editor</h2>
+                </div>
+                <MyEditor />
+            </div>
+        );
+    }
 }
 
 export default App;
